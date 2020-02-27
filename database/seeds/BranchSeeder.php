174399853262c45bc49c6f14affea8edb4115204
vahidizadyar <vahid.izadyar@gmail.com>
2020-02-27 17:33:26 +0330

    fix register blade options add branch files to git

---
+++ database/seeds/BranchSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class BranchSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table("branches")->insert([
+            "title"=>"مرکز",
+            'created_at'=>now(),
+            'updated_at'=>now(),
+        ]);
+        DB::table("branches")->insert([
+            "title"=>"اصفهان",
+            'created_at'=>now(),
+            'updated_at'=>now(),
+        ]);
+        DB::table("branches")->insert([
+            "title"=>"خراسان",
+            'created_at'=>now(),
+            'updated_at'=>now(),
+        ]);
+        DB::table("branches")->insert([
+            "title"=>"شمالغرب",
+            'created_at'=>now(),
+            'updated_at'=>now(),
+        ]);
+    }
+}
